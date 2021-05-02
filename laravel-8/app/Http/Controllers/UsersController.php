50440e15d3bda7ea57b8d52d2883a44186e78180
WCB <webcodingby@yandex.by>
2021-05-02 20:19:42 +0300

    readme

--- laravel-8/app/Http/Controllers/UsersController.php
+++ laravel-8/app/Http/Controllers/UsersController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRequest;
 use App\Models\User;
-use Database\Factories\UserFactory;
-use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
